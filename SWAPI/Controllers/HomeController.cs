1fbc0923ad88f3957eaf37fe0b08c46ab31c8098
Cameron Dziesinski <cdziesinski@gmail.com>
2020-03-04 02:02:38 -0500

    initial push

---
+++ SWAPI/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SWAPI.Models;
+
+namespace SWAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public async Task<IActionResult> GetPlanetById(int id)
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://swapi.co/api/");
+            var response = await client.GetAsync($"planets/{id}");
+            var planet = await response.Content.ReadAsAsync<Results>();
+            return View(planet);
+        }
+        public async Task<IActionResult> GetPeopleById(int id)
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://swapi.co/api/");
+            var response = await client.GetAsync($"people/{id}");
+            var people = await response.Content.ReadAsAsync<Result>();
+            return View(people);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+
+}
